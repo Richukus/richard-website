8073c0f4e3f010d6f1ad591a09187daf9e0f7680
Richard McReynolds <richardmcreynolds@Mac.lan>
2026-07-28 11:02:29 +0100

    Changed navbar to different file so its unified.  Started adding other pages

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,11 @@
-const navbar = document.getElementById("navbar");
+function initNavigation() {
+  const navbar = document.getElementById("navbar");
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 120) {
-    navbar.classList.add("show");
-  } else {
-    navbar.classList.remove("show");
-  }
-});
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 120) {
+      navbar.classList.add("show");
+    } else {
+      navbar.classList.remove("show");
+    }
+  });
+}
