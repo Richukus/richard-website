e597acab559644d156f33df99abd32d32e2d3e99
Richard McReynolds <richardmcreynolds@Mac.lan>
2026-08-10 08:55:09 +0100

    Added shared footer structure:

--- js/loadComponents.js
+++ js/loadComponents.js
@@ -1,5 +1,5 @@
 async function loadNavbar() {
-  const response = await fetch("/components/navbar.html");
+  const response = await fetch("../components/navbar.html");
   const html = await response.text();
 
   document.getElementById("navbar-placeholder").innerHTML = html;
@@ -7,4 +7,12 @@ async function loadNavbar() {
   initNavigation();
 }
 
+async function loadFooter() {
+  const response = await fetch("../components/footer.html");
+  const html = await response.text();
+
+  document.getElementById("footer-placeholder").innerHTML = html;
+}
+
 loadNavbar();
+loadFooter();
